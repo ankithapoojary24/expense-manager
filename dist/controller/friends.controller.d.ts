9d7d6b8d2c646912d414673ff87240794be20797
Ankitha Poojary <ankithau234@gmail.com>
2026-04-09 17:49:02 +0530

    Removed node_modules and dist from repo

--- dist/controller/friends.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Friend } from "../models/friend.model";
-export declare class FriendsController {
-    checkEmailExists(email: string): boolean;
-    checkPhoneExists(phone: string): boolean;
-    addFriend(friend: Friend): void;
-}
-//# sourceMappingURL=friends.controller.d.ts.map
